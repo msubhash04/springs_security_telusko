1affbc67bee5c17177a642a3232f9027462d90ce
msubhash04 <msbhose123@gmail.com>
2026-03-31 23:08:33 +0530

    Using the UserDetailsService [Spring Customized Credentials]

--- src/main/java/com/springsecurity/telusko/security/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/telusko/security/SecurityConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +20,27 @@ public class SecurityConfiguration {
     {
         http.csrf(Customizer -> Customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
+       // http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("navin")
+                .password("n@123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("sushil")
+                .password("s@123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 }
